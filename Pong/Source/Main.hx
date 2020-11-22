d54d1ec92f86468dbd0689db8ef4057dfe805414
Jiajun Ma <jiajun.nz@gmail.com>
2020-11-22 15:57:10 +1100

    Added platform and ball classes

--- Pong/Source/Main.hx
+++ Pong/Source/Main.hx
@@ -1,11 +1,140 @@
-package;
+package ;
+
+
 
 import openfl.display.Sprite;
 
-class Main extends Sprite
+import openfl.events.Event;
+
+import openfl.Lib;
+
+
+
+class Main extends Sprite 
+
 {
-	public function new()
+
+	private var platform1:Platform;
+
+	private var platform2:Platform;
+
+	private var ball:Ball;
+
+	var inited:Bool;
+
+
+
+	/* ENTRY POINT */
+
+	
+
+	function resize(e) 
+
 	{
-		super();
+
+		if (!inited) init();
+
+		// else (resize or orientation change)
+
 	}
+
+	
+
+	function init() 
+
+	{
+
+		if (inited) return;
+
+		inited = true;
+
+		
+
+		// code
+
+		// Platform 1
+		platform1 = new Platform();
+
+		platform1.x = 5;
+
+		platform1.y = 200;
+
+		this.addChild(platform1);
+
+		// Platform 2
+		platform2 = new Platform();
+
+		platform2.x = 480;
+
+		platform2.y = 200;
+
+		this.addChild(platform2);
+
+		// Ball
+		ball = new Ball();
+
+		ball.x = 250;
+
+		ball.y = 250;
+
+		this.addChild(ball);
+
+	}
+
+
+
+	/* SETUP */
+
+
+
+	public function new() 
+
+	{
+
+		super();	
+
+		addEventListener(Event.ADDED_TO_STAGE, added);
+
+	}
+
+
+
+	function added(e) 
+
+	{
+
+		removeEventListener(Event.ADDED_TO_STAGE, added);
+
+		stage.addEventListener(Event.RESIZE, resize);
+
+		#if ios
+
+		haxe.Timer.delay(init, 100); // iOS 6
+
+		#else
+
+		init();
+
+		#end
+
+	}
+
+	
+
+	public static function main() 
+
+	{
+
+		// static entry point
+
+		Lib.current.stage.align = openfl.display.StageAlign.TOP_LEFT;
+
+		Lib.current.stage.scaleMode = openfl.display.StageScaleMode.NO_SCALE;
+
+		Lib.current.addChild(new Main());
+
+		//
+
+	}
+
 }
